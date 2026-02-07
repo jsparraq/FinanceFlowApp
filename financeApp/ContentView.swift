21270f82aa79a8bac67f87dd9851d4c4b6e3f1a5
Juan Sebastian Parra Quintero <sebastian.parra96@gmail.com>
2026-02-07 10:50:15 -0500

    Initial Commit

---
+++ financeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  financeApp
+//
+//  Created by Juan Sebastian Parra Quintero on 7/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ financeApp/financeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  financeAppApp.swift
+//  financeApp
+//
+//  Created by Juan Sebastian Parra Quintero on 7/02/26.
+//
+
+import SwiftUI
+
+@main
+struct financeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
